5b32dd1c2d03c7d6ec4b7950efac3d216d0f84fe
Quentin PAYET <quentinpayet1203@gmail.com>
2022-10-17 16:34:35 +0400

    fonctionnel

--- Ressources/TEST ml5 hand gesture/TEST mediapipe hands/test2sketch.js
+++ Ressources/TEST ml5 hand gesture/TEST mediapipe hands/test2sketch.js
@@ -4,6 +4,7 @@ const videoElement = document.getElementsByClassName('input_video')[0];
         
         function onResults(results) {
           canvasCtx.save();
+        //   canvasCtx.scale(-1,-1);
           canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
           canvasCtx.drawImage(
               results.image, 0, 0, canvasElement.width, canvasElement.height);
@@ -14,110 +15,15 @@ const videoElement = document.getElementsByClassName('input_video')[0];
               drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
                              {color: '#00FF00', lineWidth: 5});
               drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 1});
-
-
-            //   console.log(results.multiHandLandmarks[0]);
-            //   console.log(results.multiHandLandmarks[1]);
-                
-            //   if(landmarks[2].y > landmarks[9].y){ //Quand mains "à l'endroit"
-            //     // console.log("actuel");
-
-            //     if (landmarks[8].y < landmarks[7].y && landmarks[6].y < landmarks[5].y){
-            //     chiffre += 1;
-            //     // console.log("index");
-            //     }
-
-            //     if (landmarks[12].y < landmarks[11].y && landmarks[10].y < landmarks[9].y){
-            //       chiffre += 1;
-            //     //   console.log("majeur");
-            //     }
-
-            //     if (landmarks[16].y < landmarks[15].y && landmarks[14].y < landmarks[13].y){
-            //       chiffre += 1;
-            //     //   console.log("annulaire");
-            //     }
-
-            //     if (landmarks[20].y < landmarks[19].y && landmarks[18].y < landmarks[17].y){
-            //       chiffre += 1;
-            //     //   console.log("auriculaire");
-            //     }
-            //     //Pouce à gauche de la mains
-            //     if((landmarks[4].x > landmarks[3].x) &&(landmarks[20].x < landmarks[4].x) && (landmarks[8].x - landmarks[0].x)-(landmarks[4].x - landmarks[0].x)<0){
-            //     //   console.log("pouce1");
-            //       chiffre += 1;
-            //     }
-            //     //Pouce à gauche de la mains
-            //     if((landmarks[4].x < landmarks[3].x) &&(landmarks[20].x > landmarks[4].x) && (landmarks[8].x - landmarks[0].x)-(landmarks[4].x - landmarks[0].x)>0){
-            //     //   console.log("pouce2");
-            //       chiffre += 1;
-            //     }
-
-            //   }
-            //   else if(true){ // Quand mains "allongé"
-            //     console.log("et ouis")
-
-            //     if(landmarks[4].y < landmarks[3].y){
-            //         console.log("pouce bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à gauche de la mains
-            //     if( (landmarks[7].x > landmarks[0].x) && (landmarks[8].x > landmarks[7].x)){
-            //         console.log("index bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à droite de la mains
-            //     if( (landmarks[7].x < landmarks[0].x) && (landmarks[8].x < landmarks[7].x)){
-            //         console.log("index bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à gauche de la mains
-            //     if( (landmarks[11].x > landmarks[0].x) && (landmarks[12].x > landmarks[11].x)){
-            //         console.log("majeur bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à droite de la mains
-            //     if( (landmarks[11].x < landmarks[0].x) && (landmarks[12].x < landmarks[11].x)){
-            //         console.log("majeur bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à gauche de la mains
-            //     if( (landmarks[15].x > landmarks[0].x) && (landmarks[16].x > landmarks[15].x)){
-            //         console.log("annulaire bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à droite de la mains
-            //     if( (landmarks[15].x < landmarks[0].x) && (landmarks[16].x < landmarks[15].x)){
-            //         console.log("annulaire bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à gauche de la mains
-            //     if( (landmarks[19].x > landmarks[0].x) && (landmarks[20].x > landmarks[19].x)){
-            //         console.log("auriculaire bleu");
-            //         chiffre += 1;
-            //     }
-            //     //Doigt à droite de la mains
-            //     if( (landmarks[19].x < landmarks[0].x) && (landmarks[20].x < landmarks[19].x)){
-            //         console.log("auriculaire bleu");
-            //         chiffre += 1;
-            //     }
-
-            //   }
-
-              
-
-            //  Afficher le resultat
-            //   console.log(chiffre);
-            //   let resultatAffiche = document.getElementById("resultatAffiche").innerHTML = chiffreTotal;
-              
             }
-
           }
 
+          //Main 1
           if (results.multiHandLandmarks[0]){
             let main1 = results.multiHandLandmarks[0];
             let chiffre1 = 0;
 
-
+            //Pos main
             if(main1[2].y > main1[9].y){ //Quand mains "à l'endroit"
                 // console.log("actuel");
 
@@ -202,15 +108,17 @@ const videoElement = document.getElementsByClassName('input_video')[0];
 
             }
 
+            //Affichage du resultat
             console.log("chiffre 1:",chiffre1);
             let resultatAffiche1 = document.getElementById("resultatAffiche1").innerHTML = chiffre1;
-
           }
 
+          //Main 2
           if (results.multiHandLandmarks[1]){
             let main2 = results.multiHandLandmarks[1];
             let chiffre2 = 0;
 
+            //Pos main
             if(main2[2].y > main2[9].y){ //Quand mains "à l'endroit"
                 // console.log("actuel");
 
@@ -295,10 +203,9 @@ const videoElement = document.getElementsByClassName('input_video')[0];
 
             }
 
+            //Affichage resultat
             console.log("chiffre 2:", chiffre2);
             let resultatAffiche2 = document.getElementById("resultatAffiche2").innerHTML = chiffre2;
-
-
           }
 
 
