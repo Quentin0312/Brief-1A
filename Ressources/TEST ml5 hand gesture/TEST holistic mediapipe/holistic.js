aa6649cb47fdf351e5d621b152de580bbd038a45
Quentin PAYET <quentinpayet1203@gmail.com>
2022-10-18 22:13:39 +0400

    holistic: fond vert fonctionnel petit artefact au niveau des mains

--- Ressources/TEST ml5 hand gesture/TEST holistic mediapipe/holistic.js
+++ Ressources/TEST ml5 hand gesture/TEST holistic mediapipe/holistic.js
@@ -9,32 +9,35 @@ function onResults(results) {
                       canvasElement.width, canvasElement.height);
 
   // Only overwrite existing pixels.
-  canvasCtx.globalCompositeOperation = 'source-in';
+  canvasCtx.globalCompositeOperation = 'source-out';
   canvasCtx.fillStyle = '#00FF00';
   canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
   // Only overwrite missing pixels.
   canvasCtx.globalCompositeOperation = 'destination-atop';
+  // flux video
   canvasCtx.drawImage(
       results.image, 0, 0, canvasElement.width, canvasElement.height);
 
   canvasCtx.globalCompositeOperation = 'source-over';
-  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                 {color: '#00FF00', lineWidth: 4});
-  drawLandmarks(canvasCtx, results.poseLandmarks,
-                {color: '#FF0000', lineWidth: 2});
-  drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
-                 {color: '#C0C0C070', lineWidth: 1});
+//   drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+//                  {color: '#00FF00', lineWidth: 4}); //body batons
+//   drawLandmarks(canvasCtx, results.poseLandmarks,
+//                 {color: '#FF0000', lineWidth: 2}); //body points
+//   drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+//                  {color: '#C0C0C070', lineWidth: 1}); //visage mesh
   drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
-                 {color: '#CC0000', lineWidth: 5});
+                 {color: '#CC0000', lineWidth: 5}); // batons main gauche
   drawLandmarks(canvasCtx, results.leftHandLandmarks,
-                {color: '#00FF00', lineWidth: 2});
+                {color: '#00FF00', lineWidth: 2});// points main gauche
   drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
-                 {color: '#00CC00', lineWidth: 5});
+                 {color: '#00CC00', lineWidth: 5}); //batons main droite
   drawLandmarks(canvasCtx, results.rightHandLandmarks,
-                {color: '#FF0000', lineWidth: 2});
+                {color: '#FF0000', lineWidth: 2}); //points main droite
   canvasCtx.restore();
+
 }
+
 
 const holistic = new Holistic({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
